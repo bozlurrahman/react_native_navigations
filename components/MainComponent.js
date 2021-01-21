7e33ad166b853fc140feb7aa602b34b5e475c985
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-01-21 10:14:31 +0600

    Navigate to a route multiple times

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,21 +1,29 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen(props) {
-    console.log('extraData: ', props.extraData);
+function HomeScreen({navigation, extraData}) {
+    console.log('extraData: ', extraData);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({navigation}) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
+            <Text>Details Screen</Text>
+            <Button
+                title="Go to Details... again"
+                onPress={() => navigation.push('Details')}
+            />
         </View>
     );
 }
