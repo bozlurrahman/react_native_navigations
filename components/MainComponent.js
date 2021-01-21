af6272325899150e6e59cd8a77067fe826bdc26c
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-01-21 10:17:58 +0600

    back navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -24,6 +24,12 @@ function DetailsScreen({navigation}) {
                 title="Go to Details... again"
                 onPress={() => navigation.push('Details')}
             />
+            <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+            <Button title="Go back" onPress={() => navigation.goBack()} />
+            <Button
+                title="Go back to first screen in stack"
+                onPress={() => navigation.popToTop()}
+            />
         </View>
     );
 }
