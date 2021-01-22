d1bbd79b59826ee3a9d29d9e1007e6928ebdf18b
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-01-22 19:43:47 +0600

    Login Authentication addition

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Main from './components/MainComponent';
+import LoginAuth from './components/LoginAuth';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Main />
+      <LoginAuth />
     );
   }
 }
