0d2c4bc690ab3349787f1cd70fa2a3f7ef2e9e92
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-01-22 19:44:28 +0600

    Back to main works

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Main from './components/MainComponent';
-import LoginAuth from './components/LoginAuth';
+// import LoginAuth from './components/LoginAuth';
 
 export default class App extends React.Component {
   render() {
     return (
-      <LoginAuth />
+      <Main />
     );
   }
 }
